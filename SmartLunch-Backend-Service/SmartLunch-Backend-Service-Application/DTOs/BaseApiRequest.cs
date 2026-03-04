d6ac43604fc2b8d446f1fe9ddfcb0417373ab56c
Tung Nguyen Thanh <TungNguyenThanh25@users.noreply.github.com>
2026-03-04 21:08:54 +0700

    update BaseAPI

--- SmartLunch-Backend-Service/SmartLunch-Backend-Service-Application/DTOs/BaseApiRequest.cs
+++ SmartLunch-Backend-Service/SmartLunch-Backend-Service-Application/DTOs/BaseApiRequest.cs
@@ -8,8 +8,8 @@ namespace SmartLunch.Backend.Service.Application.DTOs
     public class BaseApiRequest
     {
         public Guid RequestId { get; set; } = Guid.NewGuid();
-        public DateTime RequestTimestamp { get; set; } = DateTime.UtcNow;
-        public string RequestType { get; set; } = string.Empty;
-        public string RequestSource { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public string Type { get; set; } = string.Empty;
+        public string Source { get; set; } = string.Empty;
     }
 }
